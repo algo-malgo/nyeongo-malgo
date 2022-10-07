9fe4b708a4968afc860d3939d243d89ce0519813
NYeonK <skdus728@gmail.com>
2022-10-08 00:58:28 +0900

    수박수박수박수박수박수

---
+++ Programmers/Lv. 1/수박수박수박수박수박수.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = "";
+
+    for(let i=1; i<=n; i++){
+        i%2===0 ? answer+="박" : answer+="수";
+    }   
+
+    console.log(answer);
+    return answer;
+}
+
+solution(3);
+//수박수
