55724e076bc3ea71619d84c40908e078adc651e4
NYeonK <skdus728@gmail.com>
2022-11-24 22:53:01 +0900

    N개의 최소공배수

--- Programmers/Lv. 2/N개의 최소공배수.js
+++ Programmers/Lv. 2/N개의 최소공배수.js
@@ -1,39 +1,20 @@
-function solution(arr) {
-    arr.sort((a,b)=>a-b)
-    let answer = arr[0];
-    let result = [];
-    let temp = [];
-    
-    const recursion = (num) => {
-        for(let i=0; i<num.length; i++){
-            if (num[i]%answer !== 0) result.push(num[i]);
-            else if(num[i]/answer === 1) result.push(num[i]);
-            else temp.push(num[i]/answer);
-        }
+const gcd = (a, b) => {
+    while (b > 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
     }
-    recursion(arr);
-    answer = temp[0];
-    //temp = [];
-    recursion(temp);
-    console.log(answer)
-    console.log(temp)
-    console.log(result)
-    answer = 1;
-    result.forEach(el=>{
-        answer *= el;
-    })
-    console.log(answer)
+    return a;
+};
 
+const lcm = (a, b) => {
+    return (a * b) / gcd(a, b);
+};
+
+const solution = (arr) => {
+    let answer = 1;
+    for (let i = 0; i < arr.length; i++) {
+        answer = lcm(answer, arr[i]);
+    }
     return answer;
-}
-
-solution([2,6,8,14])
-
-/*
-arr	result 
-[2,6,8,14]	168
-[1,2,3]	6
-
-2 3 4 7 = 8*21 = 168
-1 2 3 = 6
-*/
+};
