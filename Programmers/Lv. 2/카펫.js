4a657247a29fd60e3e53d6b2ee70c87514563108
NYeonK <skdus728@gmail.com>
2023-04-20 00:34:23 +0900

    카펫(#18)

--- Programmers/Lv. 2/카펫.js
+++ Programmers/Lv. 2/카펫.js
@@ -1,26 +1,9 @@
 function solution(brown, yellow) {
-    let grid=[], size=[];
+  for (let y = 3; y <= (brown + yellow) / y; y++) {
+    let x = parseInt((brown + yellow) / y);
 
-    for(i=1; i<=brown+yellow; i++){
-        if((brown+yellow)%i === 0) grid.push(i)
-    }
+    if (x < y) continue;
 
-    console.log(grid);
-    grid.length%2===0 
-    ? size.push(grid[grid.length/2], grid[grid.length/2-1])
-    : size.push(grid[parseInt(grid.length/2)], grid[parseInt(grid.length/2)]);
-    
-    if(grid[grid.length/2+1] === yellow+2) size = [grid[grid.length/2+1], grid[grid.length/2-2]]
-    console.log(grid);
-    console.log(size);
-    return size;
+    if ((x - 2) * (y - 2) === yellow) return [x, y];
+  }
 }
-
-solution(18,6)
-// 18, 6 => [8, 3]
-/*
-brown	yellow	return
-10	2	[4, 3]
-8	1	[3, 3]
-24	24	[8, 6]
-*/
