03a702ed00360283e2d29b9423ca45ee879cce9b
NYeonK <skdus728@gmail.com>
2023-05-23 19:27:33 +0900

    푸드 파이트 대회(#49)

--- Programmers/Lv. 1/푸드 파이트 대회.js
+++ Programmers/Lv. 1/푸드 파이트 대회.js
@@ -1,22 +1,8 @@
 function solution(food) {
-  let answer = "",
-    temp = "";
-
-  for (let i = 0; i < food.length; i++) {
-    if (food[i] % 2 !== 0) food[i]--;
-    for (let j = 0; j < food[i] / 2; j++) {
-      answer += i;
-    }
-  }
-  temp = answer;
-  answer += 0;
-
-  for (let i = temp.length - 1; i >= 0; i--) {
-    answer += temp[i];
+  let res = "";
+  for (let i = 1; i < food.length; i++) {
+    res += String(i).repeat(Math.floor(food[i] / 2));
   }
 
-  return answer;
+  return res + "0" + [...res].reverse().join("");
 }
-
-solution([1, 3, 4, 6]); //"1223330333221"
-solution([1, 7, 1, 2]); //"111303111"
