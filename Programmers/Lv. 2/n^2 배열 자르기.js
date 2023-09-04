3b6339ae3f1aa39e41118b085afc0cd7845730da
NYeonK <skdus728@gmail.com>
2023-09-04 19:37:31 +0900

    n^2 배열 자르기

--- Programmers/Lv. 2/n^2 배열 자르기.js
+++ Programmers/Lv. 2/n^2 배열 자르기.js
@@ -1,21 +1,10 @@
 function solution(n, left, right) {
-  const arr = [],
-    result = [];
+  const result = [];
 
-  for (let i = 1; i <= n; i++) {
-    arr.push(i);
-    result.push(i);
+  while (left <= right) {
+    result.push(Math.max(Math.floor(left / n), left % n) + 1);
+    left++;
   }
 
-  while (arr.length > 1) {
-    arr.shift();
-    let k = arr[0];
-    for (let i = 0; i < n; i++) {
-      if (k > 0) result.push(arr[0]);
-      else result.push(result[i]);
-      k--;
-    }
-  }
-
-  return result.slice(left, right + 1);
+  return result;
 }
